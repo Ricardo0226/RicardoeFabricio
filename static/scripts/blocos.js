3d55ca03ff227dc4987990318b3ad932d2385251
Ricardo0226 <144167797+Ricardo0226@users.noreply.github.com>
2026-09-18 15:13:09 -0300

    Delete static directory

--- static/scripts/blocos.js
+++
@@ -1,9 +0,0 @@
-for (let i = 1; i <= 12; i++) {
-  document.write("<tr>");
-  for (let j = 1; j <= 5; j++) {
-    document.write(
-      `<td class='agenda-cell' data-dia='${j}' data-horario='${i}'></td>`
-    );
-  }
-  document.write("</tr>");
-}
